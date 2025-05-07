7c5820c8f606fee669d1006c8c28baef8de9d472
SirFrancis24 <foxtrot64.fd@gmail.com>
2025-05-07 10:54:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,6 @@ def play_redirect():
     return redirect(url_for('auth.login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
